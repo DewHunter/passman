8641ee0d235e9aa73b33980674c7837d4d72f9ab
Jorge Ramos <jaramosr@ncsu.edu>
2018-12-10 01:01:06 -0800

    Refactored into vue project form with vue-cli stuff with vuetify material implementation in vue

---
+++ passmanjs/src/components/Todo.vue
@@ -0,0 +1,24 @@
+<template>
+    <li class="todo">
+        {{ text }}
+        <v-btn
+            color="info"
+            @click="remove">
+            Done
+        </v-btn>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'Todo',
+    props: {
+        text: String
+    },
+    methods: {
+        remove: function() {
+            this.$emit('remove')
+        }
+    }
+}
+</script>
